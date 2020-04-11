3fc0d5a23be236f9264d955f54aa92254b0a8aaf
Baran Açıkgöz <baranacikgoz_33@outlook.com>
2020-04-11 15:00:00 +0300

    Updates

--- precache-manifest.00aa1f0228d6939208d54ae8122f01bc.js
+++ precache-manifest.00aa1f0228d6939208d54ae8122f01bc.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "09f296688f5c5144ef7869aff9fbe8b6",
+    "revision": "1d1b90ae3c39282e0ee26c77673b22a6",
     "url": "/Employee-Management-App/index.html"
   },
   {
-    "revision": "21e75ef61c80b4fd7b4f",
+    "revision": "fe37399c09c1d68e1aaf",
     "url": "/Employee-Management-App/static/css/2.de424728.chunk.css"
   },
   {
-    "revision": "402be36440ed0f657914",
+    "revision": "aaf55565d7ac9ee732ea",
     "url": "/Employee-Management-App/static/css/main.73b4caa3.chunk.css"
   },
   {
-    "revision": "21e75ef61c80b4fd7b4f",
-    "url": "/Employee-Management-App/static/js/2.bb817001.chunk.js"
+    "revision": "fe37399c09c1d68e1aaf",
+    "url": "/Employee-Management-App/static/js/2.981d05e5.chunk.js"
   },
   {
     "revision": "3453b8997016469371284a28c0e873e2",
-    "url": "/Employee-Management-App/static/js/2.bb817001.chunk.js.LICENSE.txt"
+    "url": "/Employee-Management-App/static/js/2.981d05e5.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "402be36440ed0f657914",
-    "url": "/Employee-Management-App/static/js/main.991a4ed7.chunk.js"
+    "revision": "aaf55565d7ac9ee732ea",
+    "url": "/Employee-Management-App/static/js/main.84ceec45.chunk.js"
   },
   {
     "revision": "8e8f39b44b47f1b71aaf",
